18789fd74e656855fea520eec9aedb1ccb91db6d
Samiylo <skryshu@gmail.com>
2021-02-01 10:32:20 -0600

    add App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+
+    const APP_ID = 'adf620f6';
+    const APP_KEY = '3354b1dfea8e0ad67b92c8f52c240bf1';
+    
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
