62bb766238250793eef6227e198b86c725d98f4b
Samiylo <skryshu@gmail.com>
2021-02-03 10:50:25 -0600

    ToDo app started

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import React,{useEffect, useState} from 'react';
-import Recipe from './components/Recipe';
+import React,{ Component} from 'react';
 
-const App = () => {
+class App extends Component {
 
-    const APP_ID = 'adf620f6';
-    const APP_KEY = '3354b1dfea8e0ad67b92c8f52c240bf1';
-    const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-    const [recipes, setRecipes] = useState([]);
-
-    useEffect( async () => {
-        getRecipes();
-    }, [])
-
-    const getRecipes = async () => {
-        const response = await fetch(exampleRequest)
-        const data = response.json();
-        setRecipes(data.hits);
+    state = { 
+        newItem: "",
+        list: []
+     }
+     
+    render() { 
+        return ( 
+            <div>
+                Hello World
+            </div>
+         );
     }
-
-    return(
-        <div>
-            <h1>Recipe App</h1>
-
-            <form className="search-form" >
-                <input className="search-bar" type="text"></input>
-                <button className="search-button" type="submit">Search</button>
-            </form>
-
-            {/* {recipes.map( recipe => (<Recipe />))} */}
-        </div>
-    )
 }
-
+ 
 export default App;
